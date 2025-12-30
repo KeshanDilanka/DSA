034523c343134d93f005d80bd0d34cc9a6fdac3a
KeshanDilanka <keshandilanka998@gmail.com>
2025-12-30 23:15:30 +0530

    Update DoublyLinkedList.java

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -1,30 +1,60 @@
-package dsa;
-
-
-
-public class DoublyLinkedList {
-
+class DoublyLinkedList {
     class Node {
         int data;
         Node prev, next;
-        Node(int d) { data = d; }
+
+        Node(int data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
     }
 
     Node head;
 
-    void insert(int data) {
+    public void insert(int data) {
         Node newNode = new Node(data);
-        newNode.next = head;
-        if (head != null) head.prev = newNode;
-        head = newNode;
-    }
 
-    void display() {
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
         Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + " ");
+        while (temp.next != null) {
             temp = temp.next;
         }
+
+        temp.next = newNode;
+        newNode.prev = temp;
     }
-    
+
+    public void delete(int key) {
+        Node temp = head;
+
+        while (temp != null && temp.data != key) {
+            temp = temp.next;
+        }
+
+        if (temp == null) return;
+
+        if (temp.prev != null) {
+            temp.prev.next = temp.next;
+        } else {
+            head = temp.next;
+        }
+
+        if (temp.next != null) {
+            temp.next.prev = temp.prev;
+        }
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
 }
