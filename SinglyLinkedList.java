a07c37348d9bdc427adf49df0eacabe50fb10ac7
KeshanDilanka <keshandilanka998@gmail.com>
2026-01-07 00:12:57 +0530

    Add files via upload

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,56 +1,29 @@
+package dsa;
 
-class SinglyLinkedList {
+
+
+public class SinglyLinkedList {
+
     class Node {
         int data;
         Node next;
-
-        Node(int data) {
-            this.data = data;
-            this.next = null;
-        }
+        Node(int d) { data = d; }
     }
 
     Node head;
 
-    public void insert(int data) {
+    void insert(int data) {
         Node newNode = new Node(data);
-
-        if (head == null) {
-            head = newNode;
-            return;
-        }
-
-        Node temp = head;
-        while (temp.next != null) {
-            temp = temp.next;
-        }
-        temp.next = newNode;
+        newNode.next = head;
+        head = newNode;
     }
 
-    public void delete(int key) {
-        if (head == null) return;
-
-        if (head.data == key) {
-            head = head.next;
-            return;
-        }
-
-        Node temp = head;
-        while (temp.next != null && temp.next.data != key) {
-            temp = temp.next;
-        }
-
-        if (temp.next != null) {
-            temp.next = temp.next.next;
-        }
-    }
-
-    public void display() {
+    void display() {
         Node temp = head;
         while (temp != null) {
-            System.out.print(temp.data + " -> ");
+            System.out.print(temp.data + " ");
             temp = temp.next;
         }
-        System.out.println("null");
     }
+    
 }
