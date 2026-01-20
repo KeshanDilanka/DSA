8c3ab8693579911e6607eafe53d32a8b32e7dfdf
KeshanDilanka <keshandilanka998@gmail.com>
2026-01-20 23:29:54 +0530

    Add files via upload

---
+++ OptimizedInsertionSortAlgorithm.java
@@ -0,0 +1,31 @@
+public class OptimizedInsertionSortAlgorithm {
+
+    static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Shift elements instead of swapping
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    static void printArray(int[] arr) {
+        for (int x : arr)
+            System.out.print(x + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8, 3, 5, 2, 4};
+
+        insertionSort(arr);
+        printArray(arr);
+    }
+}
